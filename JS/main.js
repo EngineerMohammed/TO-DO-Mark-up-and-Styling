f45d9988cdbf04ddcfe1d3c19fb484a9991c0162
EngineerMohammed <eng_abo_hussien@yahoo.com>
2022-10-09 22:02:45 +0200

    up-date1

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+// Set All Variables
 /*
   Students Tasks:
   [1] Use Sweet Alert If Input Is Empty
@@ -13,104 +14,78 @@ let theAddButton = document.querySelector(".add-task .plus");
 let tasksContainer = document.querySelector(".tasks-content");
 let tasksCount = document.querySelector(".tasks-count span");
 let tasksCompleted = document.querySelector(".tasks-completed span");
-// ==========================================
-// Foucus on the input field
+
+// When the window Loaded
 window.onload = function () {
   theInput.focus();
   createNoTasks();
 };
 
-// Check if input field is Empety
+// Crat the No Task Massage
+function createNoTasks() {
+  let msgSpan = document.createElement("span");
+  msgSpan.className = "no-tasks-massage";
+  let msgText = document.createTextNode("No Tasks To Show");
+  msgSpan.appendChild(msgText);
+
+  // append to main container
+  tasksContainer.appendChild(msgSpan);
+}
+
+// creat the task span and delet button
 theAddButton.onclick = function () {
   if (theInput.value === "") {
-    alert("###Shoud enter Value Here!!!!");
-    return false;
+    alert("######@@@@@@@@@!!!!!!");
   } else {
-    // Creat No-Task-Massage
+    // [1]check if there is notask massage
     let noTasksMsg = document.querySelector(".no-tasks-massage");
-    // Check If Span With No Tasks Message Is Exist
     if (document.body.contains(noTasksMsg)) {
-      // Remove No Tasks Message
       noTasksMsg.remove();
     }
+    // Creat the Task box
+    let mainSpanTask = document.createElement("span");
+    mainSpanTask.className = "task-box";
+    let mainText = document.createTextNode(theInput.value);
+    mainSpanTask.appendChild(mainText);
+
+    // Creat the Delete Button
+    let delELESpan = document.createElement("span");
+    delELESpan.className = "delete";
+    let delelmText = document.createTextNode("Delete");
+    delELESpan.appendChild(delelmText);
+
+    // Append Del Element To Task BOX
+    mainSpanTask.appendChild(delELESpan);
+    // APPEND The Main span which containes DelElement and task-box to taskcontainer
+    tasksContainer.appendChild(mainSpanTask);
+
+    theInput.value = "";
+    calculateTasks();
   }
-  // Creat mainSpan
-  let mainSpan = document.createElement("span");
-  mainSpan.className = "task-box";
-
-  // Creat text Node for Mainspn
-  let text = document.createTextNode(theInput.value);
-  // Append text to MAIN sPAN
-  mainSpan.appendChild(text);
-
-  // Creat delete Button
-  let delElement = document.createElement("span");
-  delElement.className = "delete";
-  // Creat delete Text
-  let delText = document.createTextNode("Delete");
-
-  // Append deltext TO del element
-  delElement.appendChild(delText);
-
-  // Append Delet-ELement To Mainspan
-  mainSpan.appendChild(delElement);
-
-  // Append All To main DIV task-box
-  tasksContainer.appendChild(mainSpan);
-
-  theInput.value = "";
-  theInput.focus();
-  calculateTasks();
 };
 
-// ==============================================================
-// Activete The Delete Button
+// Activate the Delete Button
+
 document.addEventListener("click", function (e) {
   if (e.target.className === "delete") {
     e.target.parentNode.remove();
-    if (tasksContainer.childElementCount == 0) {
-      createNoTasks();
-    }
+    calculateTasks();
   }
-  // Finish Task
+  if (tasksContainer.childElementCount == 0) {
+    createNoTasks();
+  }
   if (e.target.classList.contains("task-box")) {
-    // Toggle Class 'finished'
     e.target.classList.toggle("finished");
   }
   calculateTasks();
 });
 
-// ======================================================
-// Creat No Task Massage
-function createNoTasks() {
-  // Creat Massage Span
-  let msgSpan = document.createElement("span");
-  msgSpan.className = "no-tasks-massage";
-  // Creat msg Text
-  let msgText = document.createTextNode("No-Tasks-To-Show");
-  msgSpan.appendChild(msgText);
-  // Add to taskcontainer
-  tasksContainer.appendChild(msgSpan);
-}
-
-// / Function To Calculate Tasks
-// function calculateTasks() {
-//   tasksCount.innerHTML = document.querySelectorAll(
-//     ".tasks-content .task-box"
-//   ).length;
-
-//   tasksCompleted.innerHTML = document.querySelectorAll(
-//     ".tasks-content .finished"
-//   ).length;
-// }
-
+// creat task count function
 function calculateTasks() {
-  // Calculate All Tasks
   tasksCount.innerHTML = document.querySelectorAll(
     ".tasks-content .task-box"
   ).length;
 
-  // Calculate Completed Tasks
   tasksCompleted.innerHTML = document.querySelectorAll(
     ".tasks-content .finished"
   ).length;
